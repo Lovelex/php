ed87391eab97d689bd44be8630a6b31319d6958b
Lovelex <lukas489@gmail.com>
2018-06-18 17:53:48 -0300

    muito + coisas

--- ConstantComArray.php
+++ ConstantComArray.php
@@ -1,6 +1,12 @@
 <?php
 
-define("SERVIDOR", "127.0.0.1");
+define("BANCO_DE_DADOS", [
+	'127.0.0.1',
+	'root',
+	'SENHA',
+	'NOME_DO_BANCO_DE_DADOS'
+]);
 
-echo SERVIDOR;
+print_r(BANCO_DE_DADOS);
+
 ?>
