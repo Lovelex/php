960fbdb6f63e71de0ae7a82703a24672188d4ce2
Lovelex <lukas489@gmail.com>
2018-06-19 23:19:54 -0300

    + date

---
+++ DataHora/DateTimeObject.php
@@ -0,0 +1,16 @@
+<?php 
+
+$dt = new DateTime();
+
+$periodo = new DateInterval("P15D");
+// P-Para dizer um dia/mes/ano. T-para dizer hora/minuto/segundo
+// PTM == Minutos | PM == Meses 
+echo $dt->format("d/m/Y H:i:s");
+
+$dt-> add($periodo);
+
+echo "<br>";
+
+echo $dt->format("d/m/Y H:i:s");
+
+ ?>
