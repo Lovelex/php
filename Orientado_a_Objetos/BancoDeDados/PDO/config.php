aec278e4cc84bde0188015b4967c0b917acad3f6
Lovelex <lukas489@gmail.com>
2018-06-22 14:19:22 -0300

    +

---
+++ Orientado_a_Objetos/BancoDeDados/PDO/config.php
@@ -0,0 +1,24 @@
+<?php
+
+$conn = new PDO("mysql:dbname=scrimers;host=127.0.0.1", "root", "");
+
+$stmt = $conn->prepare("SELECT * FROM tb_usuarios ORDER BY deslogin");
+
+$stmt->execute();
+
+$results = $stmt->fetchALL(PDO::FETCH_ASSOC);
+
+// Mostra todos resultados da tabela em um array multidimensional
+
+foreach ($results as $row) {
+	
+	foreach ($row as $key => $value) {
+			
+			echo ("<strong>".$key.": </strong>".$value."<br>");
+
+		}	
+
+		echo "<=========================================================><br>";
+}
+
+?>
